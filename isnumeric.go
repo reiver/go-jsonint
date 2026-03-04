d971e13fbd324f49dd20bcfca19751af13ccc0a6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-04 04:09:23 -0800

    is numeric

---
+++ isnumeric.go
@@ -0,0 +1,27 @@
+package jsonint
+
+import (
+	"unsafe"
+)
+
+func IsNumericBytes(data []byte) bool {
+	var str string = unsafe.String(unsafe.SliceData(data), len(data))
+	return IsNumericString(str)
+}
+
+func IsNumericString(data string) bool {
+	if "" == data {
+		return false
+	}
+
+	// If it is a JSON string, then remove the beginning and closing quotation-marks.
+	if '"' == data[0] {
+		if '"' != data[len(data)-1] {
+			return false
+		}
+
+		data = data[1 : len(data)-1]
+	}
+
+	return IsIntegerString(data)
+}

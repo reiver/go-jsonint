9e876896d833602cd202fab9976d08eabf2e81b1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-03-04 01:26:49 -0800

    normalize

--- int.go
+++ int.go
@@ -35,7 +35,42 @@ func Uint64(value uint64) Int {
 }
 
 func (receiver Int) MarshalJSON() ([]byte, error) {
-	return []byte(receiver.String()), nil
+	return []byte(receiver.Normalize()), nil
+}
+
+func (receiver Int) Normalize() string {
+	str := receiver.String()
+
+	if "" == str {
+		return "0"
+	}
+
+	negative := false
+
+	switch str[0] {
+	case '-':
+		negative = true
+		str = str[1:]
+	case '+':
+		str = str[1:]
+	}
+
+	if "" == str {
+		return "0"
+	}
+
+	// strip leading zeros, keeping at least one digit
+	i := 0
+	for i < len(str)-1 && str[i] == '0' {
+		i++
+	}
+	str = str[i:]
+
+	if negative && "0" != str {
+		return "-" + str
+	}
+
+	return str
 }
 
 func (receiver Int) String() string {
